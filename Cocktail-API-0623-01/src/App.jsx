4a072e953e74ef4fac253348bbe5a26d31617016
Sbencl96 <sorbhhl@gmail.com>
2023-06-20 18:06:05 +0200

    200623_01

--- Cocktail-API-0623-01/src/App.jsx
+++ Cocktail-API-0623-01/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { ChakraProvider, Image, Box} from '@chakra-ui/react'
-import Container_cocktails_cards from './Container_Card_cocktails.jsx'
+import { ChakraProvider, Image, Box, Flex, Heading} from '@chakra-ui/react'
+import Container_cards_cocktails from './Container_Card_cocktails.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +9,15 @@ function App() {
   return (
     <>
       <ChakraProvider>
-        <Box maxH='300px' overflow='hidden' bgColor='rgba(0,0,0,1)'
-        backgroundImage="url('https://images.unsplash.com/photo-1596463989140-3b600dab72e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')">
-        </Box>
-        <Container_cocktails_cards/>
+        <Flex>
+          <Box bg='#000000' maxWidth={600} justifyContent={"center"} alignContent={"center"}>
+            <Image src="./src/assets/photo-1514362545857-3bc16c4c7d1b.jpg"/>
+          </Box>
+          <Box bg='#000000' justifyContent={'right'} alignItems={'center'}>
+            <Heading size='lg' color='#ffffff'>Cocktail recipe finder</Heading>
+          </Box>
+        </Flex>
+        <Container_cards_cocktails/>
       </ChakraProvider>
     </>
   )
--- Cocktail-API-0623-01/src/Container_Card_cocktails.jsx
+++ Cocktail-API-0623-01/src/Container_Card_cocktails.jsx
@@ -2,14 +2,15 @@ import {SimpleGrid, Button, Container} from '@chakra-ui/react'
 import { useState } from 'react';
 import Card_cocktail from './Card_cocktail.jsx'
 
-function Container_cocktails_cards() {
+function Container_cards_cocktails() {
     const [data,setData] = useState();
     
 
     return (
       <>
         <Button variant='solid' colorScheme='green' onClick={() => setData(Math.random())}>Random!</Button>
-        <SimpleGrid minChildWidth='360px' spacing='60px' justifyContent={"center"} alignContent={"center"} bg='#e4f4e9' paddingX='4em'>
+        <SimpleGrid minChildWidth='360px' spacing='60px'
+        justifyContent={"center"} alignContent={"center"} bg='#e4f4e9' paddingX='4em'>
             <Card_cocktail data={data}/>
             <Card_cocktail data={data}/>
             <Card_cocktail data={data}/>
@@ -18,4 +19,4 @@ function Container_cocktails_cards() {
     )
   }
 
-export default Container_cocktails_cards;
+export default Container_cards_cocktails;
