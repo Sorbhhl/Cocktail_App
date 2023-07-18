d2e36a1a6d1ba9ddebf2f62aef1134e1add02245
Sbencl96 <sorbhhl@gmail.com>
2023-07-18 12:47:13 +0200

    180723_01
    - Fix random cocktails list.
    - Search cocktails, list all found cocktails.
    - List a minimun of 4 cocktails in random and default cocktails.
    - Layout changes.

--- Cocktail-API-0623-01/src/components/Container_Card_cocktails.jsx
+++ Cocktail-API-0623-01/src/components/Container_Card_cocktails.jsx
@@ -1,5 +1,6 @@
 import {React, useState, useEffect} from 'react';
-import {SimpleGrid, Button, useColorModeValue, Box, Tabs, TabList, Tab, TabPanels, TabPanel, Input} from '@chakra-ui/react'
+import {SimpleGrid, Button, useColorModeValue, Box, Input, Text, Flex, Divider, AbsoluteCenter} from '@chakra-ui/react'
+import {Search2Icon} from '@chakra-ui/icons'
 import Card_cocktail from './Card_cocktail.jsx'
 
 
@@ -9,13 +10,40 @@ function Container_cards_cocktails() {
   useEffect(() => {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   .then((response) => response.json())
-  .then((data) => {console.log(data);setCocktail(data)})
+  .then((data) => {console.log(data);setCardCocktailData(data)})
   }, [])
 
   //Random cocktails
-  const handleRandomize = () => {
-    setCocktail(Math.random());
+  const getRandomCocktail = async () => {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+    setCardCocktailData(data)    
   };
+
+    //Set a minimun of 4 cocktails in default and random cocktails
+    const setCardCocktailData = async (data) => {
+      if (data.drinks && data.drinks.length < 4) {
+        const additionalResponse1 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const additionalData1 = await additionalResponse1.json();
+  
+        const additionalResponse2 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const additionalData2 = await additionalResponse2.json();
+  
+        const additionalResponse3 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const additionalData3 = await additionalResponse3.json();
+  
+        const newData = {
+          ...data,
+          drinks: [...data.drinks, ...additionalData1.drinks, ...additionalData2.drinks, ...additionalData3.drinks].slice(0, 4),
+        };
+  
+        console.log(newData);
+        setCocktail(newData);
+      } else {
+        console.log(data);
+        setCocktail(data);
+      }
+    }
 
   //Search for specific drinks
   const [searchTerm, setSearchTerm] = useState("");
@@ -33,21 +61,37 @@ function Container_cards_cocktails() {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
     const data = await response.json();
     console.log(data);
-    setCocktail(data.drinks[0])
+    setCocktail(data)
   };
+
+  let numberCocktails = cocktail.drinks?.length;
 
   return (
     <>
       <Box bg={useColorModeValue('orange.50', 'blue.800')}>
-        <Button variant='solid' colorScheme='green' onClick={handleRandomize}>Random!</Button>
-        <form onSubmit={handleFormSubmit}>
-          <Input type="text" placeholder="Buscar bebidas" value={searchTerm} onChange={handleInputChange} marginRight="2"/>
-          <Button type="submit">Buscar</Button>
-        </form>
-        <SimpleGrid minChildWidth='280px' spacing='60px'
-        justifyContent={"center"} alignContent={"center"} paddingX='10px'>
-          {cocktail?.map((drink) => (
-            <Card_cocktail key={drink.idDrink} cocktail={drink} />
+        <Flex direction={{ base: "column", md: "row" }} alignItems={"center"} justifyContent={"center"} gap='30' p={4}>
+          <Box textAlign={"center"}>
+            <Text>Look for specific cocktails:</Text>
+            <form onSubmit={handleFormSubmit}>
+              <Flex>
+                <Input type="text" placeholder="Search for cocktails..." value={searchTerm} onChange={handleInputChange}/>
+                <Button type="submit"><Search2Icon></Search2Icon></Button>
+              </Flex>
+            </form>
+          </Box>
+          <Box textAlign={"center"}>
+            <Text>Look for random cocktails:</Text>
+            <Button variant='solid' colorScheme='green' onClick={getRandomCocktail}>Random!</Button>
+          </Box>
+        </Flex>
+        <Divider orientation='horizontal'></Divider>
+        <Box textAlign={"center"} p={4}>
+          <Text>Listed drinks: {numberCocktails}</Text>
+        </Box>
+        <SimpleGrid minChildWidth='280px' spacing='30px' minHeight={500}
+        justifyContent={"center"} alignContent={"center"} paddingX='60px'>
+          {cocktail.drinks?.map((cocktail, index) => (
+            <Card_cocktail key={index} cocktail={cocktail} />
           ))}
         </SimpleGrid>
       </Box>
--- Cocktail-API-0623-01/src/components/Det_row.jsx
+++ Cocktail-API-0623-01/src/components/Det_row.jsx
@@ -5,7 +5,7 @@ import { CheckIcon, StarIcon, InfoIcon} from '@chakra-ui/icons';
 function Details_row(){
     return(
         <>
-            <Flex p={10}
+            <Flex p={6}
             bg={useColorModeValue('orange.100', 'blue.900')}
             direction={{ base: "column", md: "row" }}
             alignItems={"center"}
@@ -13,15 +13,15 @@ function Details_row(){
             >
                 <Box textAlign="center" fontWeight={"light"} bg={useColorModeValue('orange.50', 'blue.800')} borderRadius="xl" p={5} margin={5}>
                     <CheckIcon/>
-                    <Text fontSize={"xl"} p={5}>Recipe cocktail finder</Text>
+                    <Text fontSize={"xl"} p={2}>Recipe cocktail finder</Text>
                 </Box>
                 <Box textAlign="center" fontWeight={"light"} bg={useColorModeValue('orange.50', 'blue.800')} borderRadius="xl" p={5} margin={5}>
                     <StarIcon/>
-                    <Text fontSize={"xl"} p={5}>Made with React, Vite and Chakra UI</Text>
+                    <Text fontSize={"xl"} p={2}>Made with React, Vite and Chakra UI</Text>
                 </Box>
                 <Box textAlign="center" fontWeight={"light"} bg={useColorModeValue('orange.50', 'blue.800')} borderRadius="xl" p={5} margin={5}>
                     <InfoIcon/>
-                    <Text fontSize={"xl"} p={5}>Consuming <Link href="https://www.thecocktaildb.com/api.php" isExternal>TheCocktailDB API</Link></Text>
+                    <Text fontSize={"xl"} p={2}>Consuming <Link href="https://www.thecocktaildb.com/api.php" isExternal>TheCocktailDB API</Link></Text>
                 </Box>
             </Flex>
         </>
